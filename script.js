3191e4a39827ce48760c127ef776c2f427dedab7
Yuko Tanaka <tanakayuko416@gmail.com>
2025-05-01 16:03:57 +0000

    Afegida funcionalitat per mostrar contactes

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("Agenda Web");
+const contactes = ["Jose","Saida","Ruben"];
+const llista = document.createElement('ul');
+contactes forEach(nom => {
+   const li = document.createElement('li');
+   li.textContent = nom;
+   llista.appendChild('li');
+});
+document.body.appendChild('llista');
